9249ad792155526e888bd9739b8ebfe9af1bb9f9
Tobeyw <tobey1024@126.com>
2023-09-15 17:26:24 +0800

    modify lib

--- contracts/solidity/Policy.sol
+++ contracts/solidity/Policy.sol
@@ -1,7 +1,7 @@
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-import "./lib/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./GovernanceVote.sol";
 
 contract Policy is GovernanceVote{
